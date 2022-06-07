3f115b9b281952a4dbda9ba1426afc4ba6a2a4df
yepeiyu <yepeiyu@crf-tech.com>
2022-06-07 15:57:37 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus'
+// element icons
+import * as Icons from "@element-plus/icons-vue";
+import 'element-plus/dist/index.css'
 import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 注册element Icons组件
+Object.keys(Icons).forEach(key => {
+	app.component(key, Icons[key as keyof typeof Icons]);
+});
+
+app.use(ElementPlus).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,46 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [vue()],
+  // server config
+  server: {
+    host: "0.0.0.0", // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
+    cors: true,
+    // https: false,
+    // 代理跨域（mock 不需要配置，这里只是个事列）
+    proxy: {
+      "/api": {
+        target: "", // 接口地址
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, "")
+      }
+    }
+  },
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
     }
   },
+  // build configure
+  build: {
+    outDir: "dist",
+    minify: "terser",
+    terserOptions: {
+      // delete console/debugger
+      compress: {
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        // Static resource classification and packaging
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+      }
+    }
+  }
 })
 
